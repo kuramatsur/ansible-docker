99530dc92968765e94c5c11aaa9e275f496149da
kuramatsur <kuramatsu@agenda.co.jp>
2014-08-05 11:03:05 +0900

    Update yum-ansible.sh

--- yum-ansible.sh
+++ yum-ansible.sh
@@ -1,3 +1,7 @@
 #/bin/bash
 yum install gcc python-devel python-crypto python-pip -y
 pip install PyCrypto==2.3
+
+rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+yum install ansible -y
+
